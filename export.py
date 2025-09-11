bf243951572f03292aed380463a43896ec1e8691
OmeTiberiusKirk <nukool@40.co.th>
2025-09-11 11:07:47 +0700

    developing

--- export.py
+++ export.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
         cursor.execute(query)
         totalNumberOfRows = cursor.fetchval()
         totalNumberOfRows = min(totalNumberOfRows - args["offset"], args["number"])
+        size = min(totalNumberOfRows, args["size"])
 
         if totalNumberOfRows <= 0:
             raise ValueError("No data.")
@@ -25,42 +26,38 @@ if __name__ == "__main__":
         print(f"totalNumberOfPage = {totalNumberOfPages}")
 
         for i in range(totalNumberOfPages):
-            size = min(totalNumberOfRows, args["size"])
-            output_csv_file = f"{args['table']}_{i * size + 1}-{size}.csv"
-            # totalNumberOfRows = totalNumberOfRows - size
-            print(output_csv_file)
-
+            output_csv_file = f"{args['table']}-{i + 1}.csv"
             # # Fetch data from the specified table
-            # query = f"""
-            # 	SELECT * FROM {args['table']}
-            # 	ORDER BY {args['order_by']} {args['sort']}
-            # 	OFFSET {i * args['size'] + args['offset']} ROWS
-            # 	FETCH NEXT {args['size'] if args['size'] < totalNumberOfRows else totalNumberOfRows} ROWS ONLY
-            # """
-            # cursor.execute(query)
+            query = f"""
+                SELECT * FROM {args['table']}
+                ORDER BY {args['order_by']} {args['sort']}
+                OFFSET {i * args['size'] + args['offset']} ROWS
+                FETCH NEXT {args['size'] if args['size'] < totalNumberOfRows else totalNumberOfRows} ROWS ONLY
+            """
+            cursor.execute(query)
 
-            # rows = []
-            # for row in cursor:
-            # 	# Clean newline characters in Python
-            # 	cleanedRows = [
-            # 		(
-            # 			str(item).replace("\n", "").replace("\r", "")
-            # 			if isinstance(item, str)
-            # 			else item
-            # 		)
-            # 		for item in row
-            # 	]
-            # 	rows.append(cleanedRows)
+            rows = []
+            for row in cursor:
+                # Clean newline characters in Python
+                cleanedRows = [
+                    (
+                        str(item).replace("\n", "").replace("\r", "")
+                        if isinstance(item, str)
+                        else item
+                    )
+                    for item in row
+                ]
+                rows.append(cleanedRows)
 
-            # # Get column names for the DataFrame
-            # columns = [column[0] for column in cursor.description]
+            # Get column names for the DataFrame
+            columns = [column[0] for column in cursor.description]
 
-            # # Create a Pandas DataFrame
-            # df = pd.DataFrame.from_records(rows, columns=columns)
+            # Create a Pandas DataFrame
+            df = pd.DataFrame.from_records(rows, columns=columns)
 
-            # # Export DataFrame to CSV
-            # df.to_csv(f"{dir}/{output_csv_file}", index=False, sep=args["sep"])
-            # print("--------- csv created successfully ---------")
+            # Export DataFrame to CSV
+            df.to_csv(f"{dir}/{output_csv_file}", index=False, sep=args["sep"])
+            print("--------- csv created successfully ---------")
     except pyodbc.Error as ex:
         print(ex)
     except Exception as e:
