05b33aa33b972f00b4745cc045669e0731d7885b
OmeTiberiusKirk <nukool@40.co.th>
2025-09-11 15:45:13 +0700

    developing

--- import.py
+++ import.py
@@ -2,11 +2,21 @@ import os
 import csv
 from utils import connectDB
 import pyodbc
+import logging
 
 if __name__ == "__main__":
+    # Configure basic logging to a file
+    logging.basicConfig(
+        filename="app.log",  # Name of the log file
+        level=logging.INFO,  # Minimum logging level to capture (e.g., INFO, DEBUG, WARNING, ERROR, CRITICAL)
+        filemode="w",  # 'a' for append (default), 'w' for overwrite
+        format="%(asctime)s - %(levelname)s - %(message)s",  # Format of log messages
+    )
+
     directory_path = "csv"  # Replace with your directory
 
     try:
+
         cnxn, cursor = connectDB()
 
         for filename in os.listdir(directory_path):
@@ -21,15 +31,18 @@ if __name__ == "__main__":
                         try:
                             insert_query = f"INSERT INTO {table_name} VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
                             cursor.execute(insert_query, *row)
-                            print(f'inserted in to {table_name}')
+                            print(f"inserted in to {table_name}")
+                            print(f"row = {row}")
                         except pyodbc.Error as e:
-                            print(e)
+                            print(e.args[1])
+                            logging.error(e.args[1])
         cnxn.commit()
         cursor.close()
+    except pyodbc.Error as e:
+        print(e)
+        logging.error(e.args[1])
     except Exception as e:
         print(e)
-    except pyodbc.Error as e:
-        print(e)   
     finally:
         if "cnxn" in locals() and cnxn:
             cnxn.close()
